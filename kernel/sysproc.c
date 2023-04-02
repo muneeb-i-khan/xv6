dd0e36bbc07b5c091197240847c1b213d78e7d54
muneeb-i-khan <muneebk015@gmail.com>
2023-04-02 17:56:07 +0530

    Added retint syscall and command

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -89,3 +89,7 @@ sys_uptime(void)
   release(&tickslock);
   return xticks;
 }
+uint64
+sys_retint(void){
+  return 2023;
+}
--- user/user.h
+++ user/user.h
@@ -22,6 +22,7 @@ int getpid(void);
 char* sbrk(int);
 int sleep(int);
 int uptime(void);
+int retint(void);
 
 // ulib.c
 int stat(const char*, struct stat*);
